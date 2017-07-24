35c2fb707fcdb699f5c562bddaff537b690c4119
Alexander Höreth <ahoereth@gmail.com>
2017-07-24 19:35:42 +0200

    Track current torcs config.

--- torcs.py
+++ torcs.py
@@ -15,25 +15,26 @@ start = time.time()
 
 model = DDPG(
     ENV_NAME, checkpoint=CHECKPOINT,
-    memory=1e6, min_memory=150,
+    memory=1e6, min_memory=33,
     update_frequency=1, state_stacksize=1,
     simulation_workers=1, train_workers=1, feed_workers=1,
-    batchsize=64,
+    batchsize=32,
     weight_decay=True,
     bias_decay=True,
     decay_scale=1e-2,
-    actor_batch_normalization=True,
-    critic_batch_normalization=True,
+    actor_batch_normalization=False,
+    critic_batch_normalization=False,
     gamma=0.99,
     critic_learning_rate=1e-3,
     actor_learning_rate=1e-4,
     tau=1e-3,
-    mu=[0., .4],
-    theta=[.6, 1.],
-    sigma=[.3, .1],
+    mu=[0., .1],  # steering, acceleration
+    theta=[.6, 1.],  # steering, acceleration
+    sigma=[.3, .1],  # steering, acceleration
+    exploration_steps=10000,
     config_name=''
 )
-model.train(100000)
+model.train(1000000)
 # model.demo()
 
 print(time.time() - start)
